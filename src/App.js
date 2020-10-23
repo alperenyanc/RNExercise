1c95fc720b51b090912d112baa5c495f580bdf53
alp <alp@omuz.com.tr>
2020-10-23 21:37:39 +0300

    update#1
    added contextAPI & added Global State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import { StyleSheet, Text, View,SafeAreaView, Button } from 'react-native'
+import React from 'react';
+import {View, StyleSheet, SafeAreaView} from 'react-native';
+ 
+import GlobalState from './modules/Context/GlobalState';
+import MyButton from './modules/MyButton'
+ import MyText from './modules/MyText'
+ import MyText2 from './modules/MyText2'
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container} >
-     <MyButton/>
-     <MyText />
+    <SafeAreaView style={{flex:1}} >
+      <GlobalState>
+        
+        <View style={styles.container}>
+          <MyButton />
+          <MyText/>
+ 
+        </View>
+      </GlobalState>
     </SafeAreaView>
-  )
+  );
 }
 
 export default App
 
+
+ 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
+    flexDirection: 'column',
     justifyContent:'center',
     alignItems:'center'
-  }
-})
-const MyText = ()=><Text>alp</Text>
-const MyButton = ()=><Button title='Press Me' onPress={()=>alert('arttırılack')} />
+  },
+   
+});
---
+++ src/modules/Context/context.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default React.createContext({
+    number:0,
+ 
+     
+    increaseNumber:(number)=>{},
+})
+const number = React.createContext(0);
+ 
